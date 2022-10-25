e8037a0d7d7c0945ccdf572032707ffc2c5fb7ab
EinarOmarVillegasRuiz <98448238+EinarOmarVillegasRuiz@users.noreply.github.com>
2022-10-24 23:36:13 -0500

    Update header.php

--- header.php
+++ header.php
@@ -5,5 +5,6 @@
     <li><a href="cifradorsa.php">Asimétrico</a></li>
     <li><a href="cifradomd5.php">Hash - V1</a></li>
     <li><a href="cifradohas1.php">Hash - V2</a></li>
-    <li><a href="cifrado_personal.php">Propio</a></li>
+    <li><a href="cifradopropio.php">Propio</a></li>
+
 </ul>
